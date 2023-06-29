b5634d2eeab8064f7d8dff3954c3483968994f46
YoshiTv <110157580+YoshiTv@users.noreply.github.com>
2023-06-29 11:47:27 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -4,19 +4,16 @@ ESX = exports["es_extended"]:getSharedObject()
 ------------------------------------- PARTE SERVER ARMI -----------------------------------------
 -------------------------------------------------------------------------------------------------
 RegisterServerEvent("pistola", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'WEAPON_PISTOL', Config.QuantitaRicevere, false, false)
 end)
 
 RegisterServerEvent("pistolamk2", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'WEAPON_PISTOL_MK2', Config.QuantitaRicevere, false, false)
 end)
 
 RegisterServerEvent("pdw", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'WEAPON_COMBATPDW', Config.QuantitaRicevere, false, false)
 end)
@@ -24,19 +21,16 @@ end)
 ------------------------------------ PARTE SERVER OGGETTI ---------------------------------------
 -------------------------------------------------------------------------------------------------
 RegisterServerEvent("granata", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'WEAPON_GRENADE', Config.QuantitaRicevere, false, false)
 end)
 
 RegisterServerEvent("armour", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'ARMOUR', Config.QuantitaRicevere, false, false)
 end)
 
 RegisterServerEvent("parachute", function(item)
-    print(item)
     exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
     exports.ox_inventory:AddItem(source, 'PARACHUTE', Config.QuantitaRicevere, false, false)
 end)
